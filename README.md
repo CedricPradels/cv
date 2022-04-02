3caed088d78442b23b39950c149c8b8aa0adf34b
CedricPradels <cedric.pradels@gmail.com>
2022-04-02 07:15:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo
+
+- build minimal stack
+- add circleCI cfg
+- purge and deploy on s3
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { GetServerSideProps, NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Home: NextPage = () => {
+  return <Container>HelloWorld</Container>;
+};
+
+export default Home;
